87da82296d480f97fed3fe134bda0e4b287cd8c4
Lustroit404 <2290845194@qq.com>
2025-08-22 14:52:15 +0800

    Delete spark_cep/spark-cep-common directory
    Cut cuz spark was not suitable to using pure-streaming.

--- spark_cep/spark-cep-common/src/main/scala/org/apache/spark/cep/models/IPLog.scala
+++
@@ -1,6 +0,0 @@
-package org.apache.spark.cep.models
-
-case class IPLog (
-                 UserId:Int,
-                 ipAddress:String,
-                 logTime:java.sql.Timestamp)
--- spark_cep/spark-cep-common/src/main/scala/org/apache/spark/cep/models/orderEvent.scala
+++
@@ -1,9 +0,0 @@
-package org.apache.spark.cep.models
-
-import scala.util._
-import java.sql._
-
-case class orderEvent(orderId:String,
-                        userId:Int,
-                        swim_Time:java.sql.Timestamp,
-                        amount:Double)
